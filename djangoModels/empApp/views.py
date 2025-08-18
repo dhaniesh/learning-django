fd9438f8173875b3ce1d971bac0bd51a5d9bbdff
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-18 20:15:29 +0530

    Add employee list view and template rendering
    Configure Django to locate templates and define a URL route for employee data. Create a view to fetch and display all employee records using an HTML template.

--- djangoModels/empApp/views.py
+++ djangoModels/empApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from empApp.models import Employee
 # Create your views here.
+
+
+def employee_data(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees.html', {'employees': employees})

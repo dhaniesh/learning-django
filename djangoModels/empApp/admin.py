e49721c38a61dd2232618e57b9e7790936c1e8fe
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-19 21:22:58 +0530

    feat: Customize Employee list display in Django admin
    Configure the `EmployeeAdmin` to display `firstName`, `lastName`, `salary`, and `email` fields in the Django admin change list page for the Employee model. This enhances the usability and readability of the admin interface.

--- djangoModels/empApp/admin.py
+++ djangoModels/empApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from empApp.models import Employee
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['firstName', 'lastName', 'salary', 'email']
 
-admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)

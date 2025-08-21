3d9d0ffc9d857292df43e3e8bc1d7201ecbf54fc
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-21 12:00:49 +0530

    feat: Add gender field and implement form submission
    - Introduce a `gender` field with a dropdown select to the `UserRegistrationForm`.
    - Implement `POST` request handling in `userRegistrationView` to validate and process submitted form data.
    - Update the submit button text from "Register" to "Submit" in the template.

--- formsDemo/formsApp/views.py
+++ formsDemo/formsApp/views.py
@@ -3,4 +3,9 @@ from . import forms
 # Create your views here.
 def userRegistrationView(request):
     form = forms.UserRegistrationForm()
+    if request.method == 'POST':
+        form = forms.UserRegistrationForm(request.POST)
+        if form.is_valid():
+            print('Form is valid')
+            print('firstName', form.cleaned_data['firstName'])
     return render(request, 'UserRegistration.html', {'form': form}) 

426e0b8ef5fa6fa6846e11e63a90883b1d479b9c
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-21 14:05:20 +0530

    feat: Create Employee model with id, name, and salary fields
    Adds the Employee model to `firstApp/models.py`.
    Defines fields for employee ID, name, and salary.
    Implements a `__str__` method for clear object representation.

--- drf/firstProject/firstApp/models.py
+++ drf/firstProject/firstApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    sal = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self):
+        return f'Object of class Employee id:{self.id}, name:{self.name}, sal:{self.sal}'

08fb173520b00123e128e4ee7dc08d0b2a9b66ca
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-21 13:59:43 +0530

    feat: Implement basic employee API endpoint
    Introduce `firstApp` to define a `/emps/` endpoint that returns a sample JSON response.
    - Create `firstApp/urls.py` to map `/emps/` to `employeeView`.
    - Implement `employeeView` in `firstApp/views.py` to return a `JsonResponse`.
    - Include `firstApp.urls` in `firstProject/urls.py`.
    - Add `djangorestframework` as a project dependency and update `uv.lock`.

--- drf/firstProject/firstApp/views.py
+++ drf/firstProject/firstApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def employeeView(request):
+    emp = {
+        'id': 123,
+        'name': 'John',
+        'sal': '1'
+    }
+
+    return JsonResponse(emp)

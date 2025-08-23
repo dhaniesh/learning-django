0cb25721f609c44e8ee1391ed02522be7ec14ef9
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-23 15:44:26 +0530

    implement findflights endpoint

--- drf/flightServices/flightApp/views.py
+++ drf/flightServices/flightApp/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from flightApp.models import Flight, Passenger, Reservation
 from flightApp.serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+from rest_framework.decorators import api_view
+from rest_framework.views import Response
 # Create your views here.
+
+
+@api_view(['POST'])
+def findFlights(request):
+    flights = Flight.objects.filter(
+        departureCity=request.data['departureCity'],
+        arrivalCity=request.data['arrivalCity'],
+        dateOfDeparture=request.data['dateOfDeparture']
+    ).all()
+    serializer = FlightSerializer(flights, many=True)
+    return Response(serializer.data)
 
 
 class FlightViewSet(ModelViewSet):
